3abfd54b5eb96778d008971052752833f74fbf39
KenTandrian <kenricktan11@gmail.com>
2023-06-03 11:08:33 +0700

    feat: implement dark mode @ header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,19 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="flex p-5 justify-between sticky top-0 bg-white z-50 shadow-md">
-      <div className="flex space-x-2 items-center">
-        <Image src="/openai-b.png" alt="Logo" height={30} width={30} />
+    <header className="flex p-5 justify-between sticky top-0 bg-white dark:bg-zinc-900 z-50 shadow-md">
+      <div className="flex space-x-2 items-center text-black dark:text-zinc-200">
+        <Image
+          src="/openai-b.png"
+          alt="Logo"
+          height={30}
+          width={30}
+          className="dark:invert"
+        />
         <div>
           <h1 className="font-bold">
-            The <span className="text-violet-500">AI</span> Image Generator
+            The <span className="text-violet-500 dark:text-violet-400">AI</span>{" "}
+            Image Generator
           </h1>
           <h2 className="text-xs">
             Powered by DALL·E 2.0, ChatGPT & Google Cloud Functions!
@@ -22,13 +29,13 @@ const Header = () => {
           href="https://www.linkedin.com/in/kenrick-tandrian"
           target="_blank"
         >
-          <FaLinkedin className="text-gray-500 hover:text-gray-700" />
+          <FaLinkedin className="text-zinc-500 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500 transition-colors" />
         </Link>
         <Link
           href="https://github.com/KenTandrian/ai-image-generator"
           target="_blank"
         >
-          <FaGithub className="text-gray-500 hover:text-gray-700" />
+          <FaGithub className="text-zinc-500 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500 transition-colors" />
         </Link>
       </div>
     </header>
