edef6b3e8c993063ef6cb42d6a8f5c7526fb0c39
KenTandrian <kenricktan11@gmail.com>
2023-06-03 10:55:32 +0700

    feat: use icons @ header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="flex p-5 justify-between sticky top-0 bg-white z-50 shadow-md space-y-2">
+    <header className="flex p-5 justify-between sticky top-0 bg-white z-50 shadow-md">
       <div className="flex space-x-2 items-center">
         <Image
           src="https://links.papareact.com/4t3"
@@ -22,20 +23,18 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="hidden sm:flex text-xs md:text-base divide-x items-center text-gray-500">
+      <div className="hidden sm:flex text-xl items-center gap-4">
         <Link
           href="https://www.linkedin.com/in/kenrick-tandrian"
-          className="px-2 font-light text-right"
           target="_blank"
         >
-          Check out my LinkedIn!
+          <FaLinkedin className="text-gray-500 hover:text-gray-700" />
         </Link>
         <Link
           href="https://github.com/KenTandrian/ai-image-generator"
-          className="px-2 font-light"
           target="_blank"
         >
-          Github Repo
+          <FaGithub className="text-gray-500 hover:text-gray-700" />
         </Link>
       </div>
     </header>
