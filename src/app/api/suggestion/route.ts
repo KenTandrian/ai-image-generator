acf13af80937ef9c9df7ede0a430bc8d554b9de7
KenTandrian <kenricktan11@gmail.com>
2023-06-03 10:45:02 +0700

    feat: handle error @ suggestion route

--- src/app/api/suggestion/route.ts
+++ src/app/api/suggestion/route.ts
@@ -2,13 +2,26 @@ import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-export async function GET(request: Request) {
+type Return = {
+  error: boolean;
+  payload: string;
+};
+
+export async function GET() {
   // Connect to Cloud Function
   const response = await fetch(`${process.env.API_URL}/getChatGPTSuggestion`, {
     cache: "no-store",
   });
-  const textData = await response.text();
-  return new NextResponse(JSON.stringify(textData.trim()), {
-    status: 200,
-  });
+  const data: Return = await response.json();
+  if (data.error) {
+    return new NextResponse(
+      JSON.stringify("Sorry, I can't think of anything right now :("),
+      { status: 500 }
+    );
+  }
+  return new NextResponse(
+    // Remove leading dot if present and trim the string
+    JSON.stringify(data.payload?.replace(/^\./, "").trim()),
+    { status: 200 }
+  );
 }
