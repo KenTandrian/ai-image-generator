f0d160878be3f3cf95ee9320e57b3c7a126c2556
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-07-01 22:19:47 +0700

    fix(sh): change major versioning

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,7 +3,7 @@
 CURRENT_VERSION=$(npm pkg get version | tr -d '"')
 GIT_COUNT=$(git rev-list --count HEAD)
 
-MAJOR=$(expr $(( $GIT_COUNT )) / 1000 + 1)
+MAJOR=$(expr $(( $GIT_COUNT )) / 1000)
 MINOR=$(expr $(( $GIT_COUNT )) % 1000 / 100)
 PATCH=$(expr $(( $GIT_COUNT )) % 1000 % 100 + 1)
 NEW_VERSION=$MAJOR.$MINOR.$PATCH
