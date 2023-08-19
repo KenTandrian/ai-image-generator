f19a28a8ae87f8befab6954e4ca28d223aca6226
KenTandrian <kenricktan11@gmail.com>
2023-08-19 19:05:30 +0700

    feat(firebase): initialize firebase functions

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFunctions } from "firebase/functions";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Functions
+const REGION = process.env.FUNCTIONS_REGION;
+export const functions = getFunctions(app, REGION);
