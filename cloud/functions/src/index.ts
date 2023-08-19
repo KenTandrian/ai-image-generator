16360495c4545167d449f4cab3d13abfdb5a8ecf
KenTandrian <kenricktan11@gmail.com>
2023-08-19 16:05:20 +0700

    feat(func): change region to SG

--- cloud/functions/src/index.ts
+++ cloud/functions/src/index.ts
@@ -1,7 +1,9 @@
+import { setGlobalOptions } from "firebase-functions/v2";
 import { generateImage } from "./handlers/generateImage";
 import { getChatGPTSuggestion } from "./handlers/getChatGPTSuggestion";
 import { getImages } from "./handlers/getImages";
 
+setGlobalOptions({ region: "asia-southeast1" });
 exports.getChatGPTSuggestion = getChatGPTSuggestion;
 exports.generateImage = generateImage;
 exports.getImages = getImages;
