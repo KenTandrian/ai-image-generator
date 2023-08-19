51195cf1d6af2dfd5b9d3e300a52de10e546e251
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-08-19 07:47:05 +0000

    fix(syntax): add comma

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   organization: process.env.OPENAI_ORGANIZATION,
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export default openai;
