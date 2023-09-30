f6f7f63f26d95882774a17a13633db147c99aa32
KenTandrian <kenricktan11@gmail.com>
2023-09-30 23:03:14 +0700

    feat(api): add metadata on generating image

--- src/app/api/generateImage/route.ts
+++ src/app/api/generateImage/route.ts
@@ -1,13 +1,16 @@
 import { callableFn } from "@/services/firebase";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const req = await request.json();
   const prompt = req.prompt;
 
-  const response = await callableFn("generateImage")({ prompt });
+  const response = await callableFn("generateImage")({
+    prompt,
+    metadata: { ip: request.ip, geo: request.geo },
+  });
   const textData = response.data;
   return NextResponse.json(textData);
 }
