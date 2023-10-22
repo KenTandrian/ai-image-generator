7fd3bd1597573ca8ffdde14e9fadff4437858f9d
KenTandrian <kenricktan11@gmail.com>
2023-10-22 14:39:29 +0700

    feat(ai): implement provider options

--- cloud/functions/src/handlers/getChatGPTSuggestion.ts
+++ cloud/functions/src/handlers/getChatGPTSuggestion.ts
@@ -2,18 +2,32 @@ import { logger as log } from "firebase-functions/v2";
 import { onCall } from "firebase-functions/v2/https";
 import { GLOBAL_OPTIONS } from "../constants";
 import OpenAIService from "../lib/openai";
+import VertexAIService from "../lib/vertexai";
 
-export const getChatGPTSuggestion = onCall(GLOBAL_OPTIONS, async () => {
+type Provider = "openai" | "vertexai";
+
+export const getChatGPTSuggestion = onCall(GLOBAL_OPTIONS, async ({ data }) => {
   try {
-    const openai = new OpenAIService();
-    const resp = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      prompt:
-        "Write a random text prompt under 30 words for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white, etc.",
-      max_tokens: 100,
-      temperature: 0.8,
-    });
-    const responseText = resp.choices[0].text.trim().replace(/"/g, "");
+    // Default to Vertex AI
+    const provider: Provider = data.provider ?? "vertexai";
+
+    let responseText = "";
+    if (provider === "openai") {
+      const openai = new OpenAIService();
+      const resp = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt:
+          "Write a random text prompt under 30 words for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white, etc.",
+        max_tokens: 100,
+        temperature: 0.8,
+      });
+      responseText = resp.choices[0].text.trim().replace(/"/g, "");
+    } else if (provider === "vertexai") {
+      const vertexai = new VertexAIService();
+      responseText = await vertexai.suggestion();
+    } else {
+      return { error: true, payload: "Invalid provider" };
+    }
     return { error: false, payload: responseText };
   } catch (err) {
     log.error(err);
