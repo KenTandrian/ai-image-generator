186f5cddcfb7b5175c46ec20e4ca83ffe716ea52
KenTandrian <kenricktan11@gmail.com>
2024-03-10 23:17:16 +0700

    feat(trpc): improve error handling

--- src/server/routes/generateImage.ts
+++ src/server/routes/generateImage.ts
@@ -9,14 +9,19 @@ type Input = z.infer<typeof generateImageInput>;
 
 export async function generateImageFn(ctx: Context, input: Input) {
   const prompt = input.prompt;
-  const response = await callableFn("generateImage")({
-    prompt,
-    metadata: { ip: ctx.ip, geo: ctx.geo },
-  });
-  if (Object.hasOwn(response.data as object, "error")) {
-    const data = response.data as { error: { message: string } };
-    return new APIResp(data.error.message, false);
+  try {
+    const response = await callableFn("generateImage")({
+      prompt,
+      metadata: { ip: ctx.ip, geo: ctx.geo },
+    });
+    if (Object.hasOwn(response.data as object, "error")) {
+      const data = response.data as { error: { message: string } };
+      return new APIResp(data.error.message, false);
+    }
+    const textData = response.data as string;
+    return new APIResp(textData, true);
+  } catch (err) {
+    if (err instanceof Error) return new APIResp(err.message, false);
+    else return new APIResp("Something went wrong", false);
   }
-  const textData = response.data as string;
-  return new APIResp(textData, true);
 }
