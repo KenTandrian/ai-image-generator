cff594f3da51e44c4548b724913e42553f821525
KenTandrian <kenricktan11@gmail.com>
2024-03-31 13:21:02 +0700

    feat(ai): add Gemini functionality

--- cloud/functions/src/handlers/getPromptSuggestion.ts
+++ cloud/functions/src/handlers/getPromptSuggestion.ts
@@ -1,28 +1,26 @@
 import { logger as log } from "firebase-functions/v2";
 import { onCall } from "firebase-functions/v2/https";
 import { GLOBAL_OPTIONS } from "../constants";
-import OpenAIService from "../lib/openai";
+import GeminiService from "../lib/gemini";
 import VertexAIService from "../lib/vertexai";
 
-type Provider = "openai" | "vertexai";
+type Provider = "gemini" | "palm2" | "openai";
 
 export const getPromptSuggestion = onCall(GLOBAL_OPTIONS, async ({ data }) => {
   try {
-    // Default to Vertex AI
-    const provider: Provider = data.provider ?? "vertexai";
+    // Default to PaLM 2
+    const provider: Provider = data.provider ?? "palm2";
 
     let responseText = "";
     if (provider === "openai") {
-      const openai = new OpenAIService();
-      const resp = await openai.completions.create({
-        model: "gpt-3.5-turbo-instruct",
-        prompt:
-          "Write a random text prompt under 30 words for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white, etc.",
-        max_tokens: 100,
-        temperature: 0.8,
-      });
-      responseText = resp.choices[0].text.trim().replace(/"/g, "");
-    } else if (provider === "vertexai") {
+      // ChatGPT is disabled for now due to billing changes.
+      return { error: true, payload: "Sorry, OpenAI is not supported." };
+    } else if (provider === "gemini") {
+      // Gemini 1.0 Pro model
+      const gemini = new GeminiService();
+      responseText = await gemini.suggestion();
+    } else if (provider === "palm2") {
+      // PaLM 2 for Chat (chat-bison) model
       const vertexai = new VertexAIService();
       responseText = await vertexai.suggestion();
     } else {
---
+++ cloud/functions/src/lib/gemini.ts
@@ -0,0 +1,46 @@
+import {
+  VertexAI,
+  type GenerativeModel,
+  type Part,
+} from "@google-cloud/vertexai";
+
+/** Google Cloud Gemini API Service */
+export default class GeminiService {
+  private client: GenerativeModel;
+  private project = process.env.GCLOUD_PROJECT;
+  private location = "asia-southeast1";
+  private model = "gemini-1.0-pro-001";
+
+  /** Initialize Gemini API Service */
+  constructor() {
+    const vertexAI = new VertexAI({
+      project: this.project!,
+      location: this.location,
+    });
+    this.client = vertexAI.getGenerativeModel({
+      model: this.model,
+    });
+  }
+
+  /** Get prompt suggestion */
+  async suggestion() {
+    const context =
+      "You are going to chat with DALL·E, an AI that generates images from text prompts. Always start the prompt with a capital letter.";
+    const example =
+      "For example, if you are asked to 'Write a random text prompt under 50 words for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white, etc.', the response could be 'Create a modern, oil painting of a futuristic city skyline at night, with a high-tech transportation system and neon lights illuminating the bustling streets below'";
+    const q =
+      "Now, please write one sentence of a random text prompt under 50 words for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white, etc.";
+    const textPart: Part = {
+      text: [context, example, q].join(" "),
+    };
+
+    const responseStream = await this.client.generateContentStream({
+      contents: [{ role: "user", parts: [textPart] }],
+    });
+
+    // Wait for the response stream to complete
+    const aggResp = await responseStream.response;
+    const prediction = aggResp.candidates[0].content.parts[0].text;
+    return prediction ?? "";
+  }
+}
