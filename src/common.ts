7ba974dd10e55cb0dece80424c28382615336f41
KenTandrian <kenricktan11@gmail.com>
2024-08-22 01:00:38 +0700

    fix(ui): missing version

--- src/common.ts
+++ src/common.ts
@@ -1,2 +1,4 @@
+import pkg from "../package.json";
+
 export const TRPC_TOKEN = process.env.NEXT_PUBLIC_TRPC_TOKEN;
-export const VERSION = process.env.npm_package_version;
+export const VERSION = process.env.npm_package_version || pkg.version;
