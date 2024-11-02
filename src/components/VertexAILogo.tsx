b90b88f23f40b37cd8753c1518dcd61b8e14fc69
KenTandrian <kenricktan11@gmail.com>
2024-11-02 14:37:40 +0700

    feat(logo): update Vertex AI logo

--- src/components/VertexAILogo.tsx
+++ src/components/VertexAILogo.tsx
@@ -4,58 +4,68 @@ export default function VertexAILogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="24px"
-      height="24px"
+      viewBox="0 0 512 512"
+      width="512px"
+      height="512px"
       {...props}
     >
-      <path
-        d="M20,13.89A.77.77,0,0,0,19,13.73l-7,5.14v.22a.72.72,0,1,1,0,1.43v0a.74.74,0,0,0,.45-.15l7.41-5.47A.76.76,0,0,0,20,13.89Z"
-        style={{ fill: "#669df6" }}
-      />
-      <path
-        d="M12,20.52a.72.72,0,0,1,0-1.43h0v-.22L5,13.73a.76.76,0,0,0-1,.16.74.74,0,0,0,.16,1l7.41,5.47a.73.73,0,0,0,.44.15v0Z"
-        style={{ fill: "#aecbfa" }}
-      />
-      <path
-        d="M12,18.34a1.47,1.47,0,1,0,1.47,1.47A1.47,1.47,0,0,0,12,18.34Zm0,2.18a.72.72,0,1,1,.72-.71A.71.71,0,0,1,12,20.52Z"
-        style={{ fill: "#4285f4" }}
-      />
-      <path
-        d="M6,6.11a.76.76,0,0,1-.75-.75V3.48a.76.76,0,1,1,1.51,0V5.36A.76.76,0,0,1,6,6.11Z"
-        style={{ fill: "#aecbfa" }}
-      />
-      <circle cx="5.98" cy="12" r="0.76" style={{ fill: "#aecbfa" }} />
-      <circle cx="5.98" cy="9.79" r="0.76" style={{ fill: "#aecbfa" }} />
-      <circle cx="5.98" cy="7.57" r="0.76" style={{ fill: "#aecbfa" }} />
-      <path
-        d="M18,8.31a.76.76,0,0,1-.75-.76V5.67a.75.75,0,1,1,1.5,0V7.55A.75.75,0,0,1,18,8.31Z"
-        style={{ fill: "#4285f4" }}
-      />
-      <circle cx="18.02" cy="12.01" r="0.76" style={{ fill: "#4285f4" }} />
-      <circle cx="18.02" cy="9.76" r="0.76" style={{ fill: "#4285f4" }} />
-      <circle cx="18.02" cy="3.48" r="0.76" style={{ fill: "#4285f4" }} />
-      <path
-        d="M12,15a.76.76,0,0,1-.75-.75V12.34a.76.76,0,0,1,1.51,0v1.89A.76.76,0,0,1,12,15Z"
-        style={{ fill: "#669df6" }}
-      />
-      <circle cx="12" cy="16.45" r="0.76" style={{ fill: "#669df6" }} />
-      <circle cx="12" cy="10.14" r="0.76" style={{ fill: "#669df6" }} />
-      <circle cx="12" cy="7.92" r="0.76" style={{ fill: "#669df6" }} />
-      <path
-        d="M15,10.54a.76.76,0,0,1-.75-.75V7.91a.76.76,0,1,1,1.51,0V9.79A.76.76,0,0,1,15,10.54Z"
-        style={{ fill: "#4285f4" }}
-      />
-      <circle cx="15.01" cy="5.69" r="0.76" style={{ fill: "#4285f4" }} />
-      <circle cx="15.01" cy="14.19" r="0.76" style={{ fill: "#4285f4" }} />
-      <circle cx="15.01" cy="11.97" r="0.76" style={{ fill: "#4285f4" }} />
-      <circle cx="8.99" cy="14.19" r="0.76" style={{ fill: "#aecbfa" }} />
-      <circle cx="8.99" cy="7.92" r="0.76" style={{ fill: "#aecbfa" }} />
-      <circle cx="8.99" cy="5.69" r="0.76" style={{ fill: "#aecbfa" }} />
-      <path
-        d="M9,12.73A.76.76,0,0,1,8.24,12V10.1a.75.75,0,1,1,1.5,0V12A.75.75,0,0,1,9,12.73Z"
-        style={{ fill: "#aecbfa" }}
-      />
+      <defs>
+        <style>
+          {`
+            .cls-1 { fill: none; }
+            .cls-1, .cls-2, .cls-3, .cls-4, .cls-5 { stroke-width: 0px; }
+            .cls-2 { fill: #34a853; }
+            .cls-3 { fill: #4285f4; }
+            .cls-4 { fill: #ea4335; }
+            .cls-5 { fill: #fbbc04;}
+          `}
+        </style>
+      </defs>
+      <g id="bounding_box" data-name="bounding box">
+        <rect
+          id="bounding_box-2"
+          data-name="bounding box-2"
+          className="cls-1"
+          width="512"
+          height="512"
+        />
+      </g>
+      <g id="art_layer" data-name="art layer">
+        <path
+          className="cls-4"
+          d="M128,249c-8.8,0-16-7.2-16-16v-105c0-8.8,7.2-16,16-16s16,7.2,16,16v105c0,8.8-7.2,16-16,16Z"
+        />
+        <path
+          className="cls-5"
+          d="M256,464c-3,0-6-.8-8.6-2.5l-176-112c-7.5-4.7-9.7-14.6-4.9-22.1,4.8-7.5,14.6-9.6,22.1-4.9l167.4,106.5,167.4-106.5c7.5-4.7,17.3-2.5,22.1,4.9,4.7,7.5,2.5,17.3-4.9,22.1l-176,112c-2.6,1.7-5.6,2.5-8.6,2.5h0Z"
+        />
+        <path
+          className="cls-2"
+          d="M256,394c-8.8,0-16-7.2-16-16v-73.1c0-8.8,7.2-16,16-16s16,7.2,16,16v73.1c0,8.8-7.2,16-16,16Z"
+        />
+        <circle className="cls-4" cx="128" cy="64" r="16" />
+        <circle className="cls-4" cx="128" cy="297" r="16" />
+        <path
+          className="cls-3"
+          d="M384.2,314c-8.8,0-16-7.1-16-16l-.2-106c0-8.8,7.1-16,16-16h0c8.8,0,16,7.1,16,16l.2,106c0,8.8-7.1,16-16,16h0Z"
+        />
+        <circle className="cls-3" cx="384" cy="64" r="16" />
+        <circle className="cls-3" cx="384" cy="128" r="16" />
+        <path
+          className="cls-5"
+          d="M320,225c-8.8,0-16-7.2-16-16v-103c0-8.8,7.2-16,16-16s16,7.2,16,16v103c0,8.8-7.2,16-16,16Z"
+        />
+        <circle className="cls-2" cx="256" cy="177" r="16" />
+        <circle className="cls-2" cx="256" cy="241" r="16" />
+        <circle className="cls-5" cx="320" cy="273" r="16" />
+        <circle className="cls-5" cx="320" cy="337" r="16" />
+        <path
+          className="cls-5"
+          d="M192,225c-8.8,0-16-7.2-16-16v-103c0-8.8,7.2-16,16-16s16,7.2,16,16v103c0,8.8-7.2,16-16,16Z"
+        />
+        <circle className="cls-5" cx="192" cy="273" r="16" />
+        <circle className="cls-5" cx="192" cy="337" r="16" />
+      </g>
     </svg>
   );
 }
