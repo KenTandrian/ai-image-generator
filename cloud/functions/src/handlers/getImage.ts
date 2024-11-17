428ecfd99c526c7fe108db4932e6dd8fbe79ccb8
KenTandrian <kenricktan11@gmail.com>
2024-11-17 21:57:23 +0700

    fix(fn): increase memory

--- cloud/functions/src/handlers/getImage.ts
+++ cloud/functions/src/handlers/getImage.ts
@@ -1,10 +1,15 @@
 import { logger as log } from "firebase-functions/v2";
-import { onCall } from "firebase-functions/v2/https";
+import { type HttpsOptions, onCall } from "firebase-functions/v2/https";
 import { GLOBAL_OPTIONS, IMAGE_FOLDER_NAME, PROJECT_NAME } from "../constants";
 import { storage } from "../lib/firebase";
 import { prepareImage } from "../utils/prepareImage";
 
-export const getImage = onCall(GLOBAL_OPTIONS, async ({ data }) => {
+const OPTIONS: HttpsOptions = {
+  ...GLOBAL_OPTIONS,
+  memory: "512MiB",
+};
+
+export const getImage = onCall(OPTIONS, async ({ data }) => {
   try {
     const path = data.path;
     const bucket = storage.bucket();
