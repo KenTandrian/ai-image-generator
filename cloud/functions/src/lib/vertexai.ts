67d309d7d9adbba5d16bea938703ceab1c7fffdd
KenTandrian <kenricktan11@gmail.com>
2025-09-25 23:54:09 +0700

    feat(fn): update Vertex AI service

--- cloud/functions/src/lib/vertexai.ts
+++ cloud/functions/src/lib/vertexai.ts
@@ -1,23 +1,19 @@
-import { PredictionServiceClient, helpers } from "@google-cloud/aiplatform";
+import { GoogleGenAI } from "@google/genai";
 import { IMAGEN_MODELS } from "../constants";
-
-type ImgPredictionResult = {
-  mimeType: "image/png";
-  bytesBase64Encoded: string;
-};
 
 /** Google Cloud Vertex AI Service */
 export default class VertexAIService {
-  private client: PredictionServiceClient;
-  private project = process.env.GCLOUD_PROJECT;
-  private location = "asia-southeast1";
-  private publisher = "google";
+  private client: GoogleGenAI;
   private defaultImagen = IMAGEN_MODELS[0];
+  private location = "asia-southeast1";
+  private project = process.env.GCLOUD_PROJECT;
 
   /** Initialize Vertex AI Service */
   constructor() {
-    this.client = new PredictionServiceClient({
-      apiEndpoint: `${this.location}-aiplatform.googleapis.com`,
+    this.client = new GoogleGenAI({
+      location: this.location,
+      project: this.project,
+      vertexai: true,
     });
   }
 
@@ -27,26 +23,17 @@ export default class VertexAIService {
     const model =
       IMAGEN_MODELS.find((m) => m.id === modelId) ?? this.defaultImagen;
 
-    if (model.location !== this.location) {
-      this.client = new PredictionServiceClient({
-        apiEndpoint: `${model.location}-aiplatform.googleapis.com`,
-      });
-    }
-
     // Generate image
-    const [response] = await this.client.predict({
-      endpoint: `projects/${this.project}/locations/${model.location}/publishers/${this.publisher}/models/${model.id}`,
-      instances: [helpers.toValue({ prompt })!],
-      parameters: helpers.toValue({
-        sampleCount: 1,
-      }),
+    const response = await this.client.models.generateImages({
+      config: {
+        numberOfImages: 1,
+      },
+      model: model.id,
+      prompt: prompt,
     });
-    if (!response.predictions?.[0]) return "";
-    const result = helpers.fromValue(
-      response.predictions[0] as protobuf.common.IValue
-    ) as ImgPredictionResult;
+    const result = response.generatedImages?.[0]?.image;
     return {
-      bytes: result.bytesBase64Encoded ?? "",
+      bytes: result?.imageBytes ?? "",
       model: model.id,
     };
   }
