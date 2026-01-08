68e15b1f12431cda81206a5eafe738c3bb2d842e
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2026-01-08 16:37:39 +0000

    feat(fn): prefer number properties

--- cloud/functions/src/utils/prepareImage.ts
+++ cloud/functions/src/utils/prepareImage.ts
@@ -7,7 +7,7 @@ export const prepareImage = (bucket: Bucket) => (x: File) => {
   const fileMeta: GCSImageMeta | undefined = x.metadata.metadata;
   const geo = fileMeta?.geo ? JSON.parse(fileMeta.geo) : undefined;
 
-  const unixCreatedAt = parseInt(
+  const unixCreatedAt = Number.parseInt(
     x.name.split("/")?.pop()?.split("-")?.[0] ?? ""
   );
   return {
