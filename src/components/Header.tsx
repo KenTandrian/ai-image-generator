39761fd8b19032d029671d638cc05910f73e8445
KenTandrian <kenricktan11@gmail.com>
2026-08-25 16:22:33 +0700

    feat(layout): simplify header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,32 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { VERSION } from "@/common";
-import AuthButton from "./AuthButton";
-import VertexAILogo from "./VertexAILogo";
+import AuthButton from "@/components/AuthButton";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white p-5 shadow-md dark:bg-zinc-900">
-      <div className="max-w-screen-3xl mx-auto flex justify-between">
-        <div className="flex items-center space-x-2 text-black dark:text-zinc-200">
-          <VertexAILogo height={42} width={42} />
-          <div>
-            <h1 className="font-bold">
-              The{" "}
-              <span className="text-violet-500 dark:text-violet-400">AI</span>{" "}
-              Image Generator
-              <span className="ml-1 rounded bg-violet-100 px-1 py-0.5 align-middle text-[0.625rem] font-medium text-violet-800 dark:bg-violet-900 dark:text-violet-300">
-                {VERSION}
-              </span>
-            </h1>
-            <h2 className="text-xs">
-              Powered by Imagen, Vertex AI & Google Cloud Functions!
-            </h2>
-          </div>
-        </div>
+    <header className="flex items-center h-16 border-b border-border justify-between">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2" />
+        <div>Image Gallery</div>
+      </div>
 
-        <div className="hidden items-center gap-4 text-xl sm:flex">
-          <Link
-            href="https://www.linkedin.com/in/kenrick-tandrian"
-            target="_blank"
-          >
-            <FaLinkedin className="text-zinc-500 transition-colors hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500" />
-          </Link>
-          <Link
-            href="https://github.com/KenTandrian/ai-image-generator"
-            target="_blank"
-          >
-            <FaGithub className="text-zinc-500 transition-colors hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500" />
-          </Link>
-          <AuthButton />
-        </div>
+      <div className="hidden items-center gap-4 text-xl sm:flex px-4">
+        <Link
+          href="https://www.linkedin.com/in/kenrick-tandrian"
+          target="_blank"
+        >
+          <FaLinkedin className="text-zinc-500 transition-colors hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500" />
+        </Link>
+        <Link
+          href="https://github.com/KenTandrian/ai-image-generator"
+          target="_blank"
+        >
+          <FaGithub className="text-zinc-500 transition-colors hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-500" />
+        </Link>
+        <AuthButton />
       </div>
     </header>
   );
